2d3849473be9b79f17fde1aed3b0625ce977dd69
Oliver <loliverhennigh101@gmail.com>
2026-03-23 13:35:16 -0700

    Drop out-of-scope derivative and ball-pivoting files from runtime split

--- physicsnemo/nn/functional/geometry/point_cloud_ball_pivoting/_warp_impl/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 - 2026 NVIDIA CORPORATION & AFFILIATES.
-# SPDX-FileCopyrightText: All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-from .op import point_cloud_ball_pivoting_warp
-
-__all__ = ["point_cloud_ball_pivoting_warp"]
